a5c34d5950308c88fa17efa368b3e86bc90af8bb
Artem Vanty <artem.vanty.0358@gmail.com>
2023-03-25 15:40:06 +0900

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function sleep(timeout) { setTimeout(null,timeout); }
+
+function sleep(event, timeout) { setTimeout(event,timeout); }
+
+function Introduction()
+{
+    alert("Добро пожаловать в игру \"ОбычнаяРаботаПрограммиста\".");
+    alert("Здесь вам придётся заниматься всем чем угодно кроме непосредственной разработки ПО.");
+    alert("Удачи!");
+}
+
+function Level1()
+{
+    alert(
+    "Дядя Валера: Слушай малец, у меня перестал работать пульт от ТВ можешь посмотреть что с ним?"
+    );
+}
+
+//Introduction();
+
+Level1();
